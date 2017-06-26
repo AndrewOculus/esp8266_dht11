b9851bef705bcb57ef8c04fd86f00f0e2f662c7b
AndrewOculus <andrewoculus@gmail. com>
2017-06-26 15:51:31 +0300

    add some code and comments

--- ESP8266_dht11_.ino
+++ ESP8266_dht11_.ino
@@ -1,30 +1,37 @@
-//https://github.com/adidax/dht11
+https://github.com/adidax/dht11
 #include <dht11.h>
+https://github.com/ekstrand/ESP8266wifi
 #include <ESP8266WiFi.h>
 
-const char* ssid     = "TP-LINK";
-const char* password = "pass";
-const char* host = "smarthome.hostapp.com";
-const int refreshTime = 5000;
-dht11 DHT;
-int timer = 0;
-#define DHT11_PIN 2
+#define DHT11_PIN 2//data pin for dth11
+
+const char* ssid  = "TP-LINK";//you wifi name
+const char* password = "pass";//your wifi pass
+const char* host = "smarthome.hostapp.com";//your hosting adress
+const unsigned long refreshTime = 5000; //send data to server after 5 second
+unsigned long old_time;//vars for update
+unsigned long new_time;//delta time 
+dht11 DHT;//dht11 lib object
+unsigned long timer = 0;//trigger timer
+
 
 void setup(){
+  //begin serial port
   Serial.begin(9600);
- 
+  //begin wi-fi connection
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   
   while (WiFi.status() != WL_CONNECTED)
   {
-    delay(500);
+    delay(1000);
   }
 }
 
 void loop(){
-  timer+=1;
-  
+  new_time = millis();
+  unsigned long delta = new_time - old_time;
+  timer+=delta;
   if(timer > refreshTime)
   {
   	//reset timer
@@ -46,7 +53,7 @@ void loop(){
 
     	String stringTemp =  String(DHT.temperature, DEC); 
     	String stringHumid = String(DHT.humidity , DEC);
-    	String str = "test= Temp: "+stringOne + " Humid: " + stringHumid + " Reliability: " + chk;
+    	String str = "temp="+stringTemp; //+ " Humid: " + stringHumid + " Reliability: " + chk;
     	client.print(str);
     	client.println( " HTTP/1.1");
     	client.print( "Host:" );
@@ -58,8 +65,6 @@ void loop(){
   	}
 
   }
-  
-
- 
+ old_time = new_time;
 }
 
